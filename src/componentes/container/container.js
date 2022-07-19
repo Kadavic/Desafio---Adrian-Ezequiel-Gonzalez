5f4b1858b574e5f51a68b504bc913be7a528d37b
Adriang <adriangezequiel@gmail.com>
2022-07-19 01:12:17 -0300

    agregando Navbar

---
+++ src/componentes/container/container.js
@@ -0,0 +1,22 @@
+/*function container(){
+    return <div style={{
+        borderColor: '#f00'
+    }}>Hola mundo!</div>
+}*/
+
+/*
+const estilos ={
+    borderColor: '#f00'
+}
+function container(){
+    return <div style={{estilos}}>Hola mundo!</div>
+}*/
+
+function Container(){
+    return(
+        <div>
+            hola mundo
+        </div>
+    );
+}
+export default Container
